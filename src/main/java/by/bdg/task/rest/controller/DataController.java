e5cd090dd1a65905e3e8770e6e54bcae6c204062
viktarkavalchuk <v.kavalchuk90@gmail.com>
2022-06-29 18:05:49 +0300

    Data controller has been created

--- src/main/java/by/bdg/task/rest/controller/DataController.java
+++ src/main/java/by/bdg/task/rest/controller/DataController.java
@@ -9,10 +9,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.NoResultException;
+import java.util.Date;
 import java.util.List;
 
 
@@ -32,164 +33,41 @@ public class DataController {
         this.converter = converter;
     }
 
-    @GetMapping("/getAll")
+    @GetMapping("/getAllData")
     public ResponseEntity<?> getAllData() {
         List<Data> dataList = (List<Data>) dataRepository.findAll();
         return new ResponseEntity<>(dataList.stream().map(d -> converter.convertToDto(d, DataDto.class)), HttpStatus.OK);
     }
 
 
-//    @GetMapping("/getByName")
-//    public ResponseEntity<?> getAnnouncementByName(@RequestParam String name) {
-//        List<Announcement> getByName = announcementService.getAll()
-//                .stream().filter(x -> x.getName().equalsIgnoreCase(name)).collect(Collectors.toList());
-//
-//        return new ResponseEntity<>(getByName.stream().map(d -> converter.convertToDto(d, AnnouncementDto.class))
-//                .collect(Collectors.toList()),
-//                HttpStatus.OK);
-//    }
+    @PostMapping
+    public ResponseEntity<?> createData(@RequestParam(value = "date", required = false) Date date,
+                                                @RequestParam(value = "text", required = false) String text,
+                                                @RequestParam(value = "image", required = false) String image,
+                                                @RequestParam(value = "number", required = false) Integer number) {
 
+        Data newData = new Data();
+        newData.setDate(date);
+        newData.setText(text);
+        newData.setImage(image);
+        newData.setNumber(number);
 
-//    @GetMapping("/getByNameAndPrice")
-//    public ResponseEntity<?> getAnnouncementByNameAndPrice(@RequestParam String name, Integer price) {
-//        List<Announcement> getByNameAndPrice = announcementService.getAll()
-//                .stream().filter(x -> x.getName().equalsIgnoreCase(name))
-//                .filter(x -> x.getPrice() < price)
-//                .collect(Collectors.toList());
-//
-//        return new ResponseEntity<>(getByNameAndPrice.stream().map(d -> converter.convertToDto(d, AnnouncementDto.class))
-//                .collect(Collectors.toList()),
-//                HttpStatus.OK);
-//    }
-//
-//
-//    @GetMapping("/getByPriceLowerThan")
-//    public ResponseEntity<?> getAnnouncementByPrice(@RequestParam Integer price) {
-//        List<Announcement> getByPrice = announcementService.getAll()
-//                .stream().filter(x -> x.getPrice() < price).collect(Collectors.toList());
-//
-//        return new ResponseEntity<>(getByPrice.stream().map(d -> converter.convertToDto(d, AnnouncementDto.class))
-//                .collect(Collectors.toList()),
-//                HttpStatus.OK);
-//    }
-//
-//
-//    @GetMapping("/getSalesHistory")
-//    public ResponseEntity<?> getAllSalesHistory() {
-//        List<Announcement> getSalesHistoryByUser = announcementService.getClosedAnnouncements();
-//
-//        return new ResponseEntity<>(getSalesHistoryByUser.stream()
-//                .map(d -> converter.convertToDto(d, AnnouncementDto.class))
-//                .collect(Collectors.toList()), HttpStatus.OK);
-//    }
-//
-//
-//    @GetMapping("/getSalesHistory/{id}")
-//    public ResponseEntity<?> getSalesHistory(@PathVariable("id") int id) {
-//        User user = userService.getById(id);
-//        List<Announcement> getSalesHistoryByUser = announcementService.getClosedAnnouncements()
-//                .stream()
-//                .filter(x -> x.getUser().getLogin().equals(user.getLogin()))
-//                .collect(Collectors.toList());
-//
-//        return new ResponseEntity<>(getSalesHistoryByUser.stream()
-//                .map(d -> converter.convertToDto(d, AnnouncementDto.class))
-//                .collect(Collectors.toList()), HttpStatus.OK);
-//    }
-//
-//
-//    @PreAuthorize("hasRole('ROLE_ADMIN')")
-//    @PatchMapping("/vip/{id}")
-//    public ResponseEntity<?> setVip(@PathVariable("id") int id,
-//                                    @RequestParam(value = "vip") Boolean vip) {
-//        logger.info("UPDATE: try to update ID Announcement: " + id);
-//        try {
-//            Announcement announcement = announcementService.getById(id);
-//            announcement.setVip(vip);
-//            announcementService.update(announcement);
-//            logger.info("UPDATE: update ID Announcement: " + id);
-//            return new ResponseEntity<>("UPDATE: update ID Announcement: " + id, HttpStatus.OK);
-//        } catch (NoResultException e) {
-//            logger.error("UPDATE: Can't update ad ID. It doesn't exist: " + id, e);
-//            return new ResponseEntity<>("UPDATE: Can't update ad ID. It doesn't exist: " + id, HttpStatus.OK);
-//        }
-//    }
-//
-//    @PreAuthorize("hasRole('ROLE_USER')")
-//    @PatchMapping("/{id}")
-//    public ResponseEntity<?> updateAnnouncement(@PathVariable("id") int id,
-//                                                @RequestParam(value = "name", required = false) String name,
-//                                                @RequestParam(value = "price", required = false) Integer price,
-//                                                @RequestParam(value = "description", required = false) String description,
-//                                                @RequestParam(value = "sold", required = false) Boolean sold) {
-//
-//        logger.info("UPDATE: try to update ID Announcement: " + id);
-//        try {
-//            User userRequest = userService.getByLogin(SecurityContextHolder.getContext().getAuthentication().getName());
-//            Announcement announcement = announcementService.getById(id);
-//
-//            if (announcement.getUser().getLogin().equalsIgnoreCase(userRequest.getLogin())) {
-//                if (name != null) {
-//                    announcement.setName(name);
-//                }
-//                if (price != null) {
-//                    announcement.setPrice(price);
-//                }
-//                if (description != null) {
-//                    announcement.setDescription(description);
-//                }
-//                if (sold != null && sold == true) {
-//                    announcement.setSold(sold);
-//                    announcement.setEndDate(new Date());
-//                } else if (sold != null && sold == false) {
-//                    announcement.setSold(sold);
-//                    announcement.setEndDate(null);
-//                }
-//                announcementService.update(announcement);
-//                logger.info("UPDATE: update succsessfully: " + id);
-//                return new ResponseEntity<>(converter.convertToDto(announcement, AnnouncementDto.class), HttpStatus.OK);
-//            } else {
-//                return new ResponseEntity<>("UPDATE: This user can`t change this announcement", HttpStatus.OK);
-//            }
-//        } catch (NoResultException e) {
-//            logger.error("UPDATE: This Announcement doesn`t exist, ID Announcement: " + id);
-//            return new ResponseEntity<>("UPDATE: This Announcement doesn`t exist, ID Announcement: " + id, HttpStatus.OK);
-//        }
-//    }
-//
-//
-//    @PreAuthorize("hasRole('ROLE_USER')")
-//    @PostMapping
-//    public ResponseEntity<?> createAnnouncement(@RequestParam(value = "name") String name,
-//                                                @RequestParam(value = "price") Integer price,
-//                                                @RequestParam(value = "description", required = false) String description) {
-//
-//        Announcement announcement = new Announcement();
-//        User userRequest = userService.getByLogin(SecurityContextHolder.getContext().getAuthentication().getName());
-//        announcement.setName(name);
-//        announcement.setPrice(price);
-//        announcement.setStartDate(new Date());
-//        announcement.setDescription(description);
-//        announcement.setUser(userRequest);
-//        announcement.setVip(false);
-//        announcement.setRating(5.0);
-//
-//        announcementService.create(announcement);
-//
-//        return new ResponseEntity<>(converter.convertToDto(announcement, AnnouncementDto.class), HttpStatus.OK);
-//    }
-//
-//    @PreAuthorize("hasRole('ROLE_ADMIN')")
-//    @DeleteMapping("/{id}")
-//    public ResponseEntity<?> delete(@PathVariable("id") int id) {
-//
-//        logger.info("DELETE: try to delete ID Announcement: " + id);
-//        try {
-//            announcementService.delete(id);
-//            logger.info("Deleted successfully, ID Announcement: " + id);
-//        } catch (NoResultException e) {
-//            logger.error("This Announcement doesn`t exist, ID Announcement: " + id, e);
-//        }
-//        return new ResponseEntity<>(HttpStatus.OK);
-//    }
+        dataRepository.save(newData);
+
+        return new ResponseEntity<>(converter.convertToDto(newData, DataDto.class), HttpStatus.OK);
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") Long id) {
+
+        logger.info("DELETE: try to delete Data by ID : " + id);
+        try {
+            dataRepository.deleteById(id);
+            logger.info("Deleted successfully, ID Data: " + id);
+        } catch (NoResultException e) {
+            logger.error("This Data doesn`t exist, ID Data: " + id, e);
+        }
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
